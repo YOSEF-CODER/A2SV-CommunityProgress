9d716ff56cd5c3e4debb0b80de8cce206447f1ab
yosef lakew <80267669+YOSEF-CODER@users.noreply.github.com>
2022-10-02 15:14:12 +0300

    Create Next Greater Element I.js

---
+++ Next Greater Element I.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var nextGreaterElement = function(nums1, nums2) {
+    let arr=[]
+    for(let i=0;i<nums1.length;i++){
+           for(let j=0;j<nums2.length;j++){
+                if(nums1[i]===nums2[j]){
+                    if(nums2[j]<nums2[j+1]){
+                        arr.push(nums2[j+1]);
+                    }else {
+                        arr.push(-1)
+                    }
+                }
+    }
+    }
+    return arr
+};
